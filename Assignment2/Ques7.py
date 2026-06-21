77aeb678c186f2266e94e53f860b4b19640ae29c
Shreeram Patidar <srpatidar6263@gmail.com>
2026-06-21 17:35:20 +0530

    Update and rename ques7.py to Assignment2/Ques7.py

---
+++ Assignment2/Ques7.py
@@ -0,0 +1,24 @@
+"""
+========================================
+Question 7: Temperature Converter
+========================================
+
+A weather application needs to convert temperature from Celsius to Fahrenheit.
+
+Write a Python program that:
+- Accepts temperature in Celsius as input.
+- Converts it to Fahrenheit using the formula:
+  F = (C × 9/5) + 32
+- Displays the result.
+
+Example:
+Celsius = 25
+Fahrenheit = 77.0
+"""
+
+principle =int(input("enter principle amount:"))
+rate = int(input("enter rate of interest:"))
+time = int(input("enter times of years"))
+
+si = principle*rate*time/100
+print(si)
--- ques7.py
+++
@@ -1,6 +0,0 @@
-principle =int(input("enter principle amount:"))
-rate = int(input("enter rate of interest:"))
-time = int(input("enter times of years"))
-
-si = principle*rate*time/100
-print(si)
