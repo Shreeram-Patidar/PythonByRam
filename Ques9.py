9f70e0dd609648a382ecb7c6efaab08ae9820118
Shreeram Patidar <srpatidar6263@gmail.com>
2026-06-21 17:16:20 +0530

    Update Ques9.py

--- Ques9.py
+++ Ques9.py
@@ -1,3 +1,7 @@
+"""
+Three Inputs Display
+Take three values from user and print each on new line.
+"""
 a=int(input("Enter First Number: "))
 b=int(input("Enter Second Number: "))
 c=int(input("Enter Third Number: "))
