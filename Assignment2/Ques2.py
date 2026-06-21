b9b6bbda3563582d6b7947617ad9dacbab5a637f
Shreeram Patidar <srpatidar6263@gmail.com>
2026-06-21 17:27:55 +0530

    Update and rename ques2.py to Assignment2/Ques2.py

---
+++ Assignment2/Ques2.py
@@ -0,0 +1,41 @@
+"""
+========================================
+Question 2: Lifetime Calculator
+========================================
+
+You are developing a feature for a health and wellness mobile app that helps users understand how long they've been alive in a more tangible way.
+
+Write a Python program that:
+- Accepts the user’s age in years as input.
+- Calculates the approximate number of:
+  Days lived (1 year = 365 days)
+  Hours lived
+  Minutes lived
+- Displays the output in the format:
+
+You've lived approximately:
+Days: xxx
+Hours: yyy
+Minutes: zzz
+
+Sample Input:
+Enter your age in years: 18
+
+Sample Output:
+You've lived approximately:
+Days: 6570
+Hours: 157680
+Minutes: 9460800
+
+"""
+
+age=int(input("enter your age in years:"))
+days = age*365
+hours =days*24
+minutes = hours*1440
+seconds = minutes*86400
+print("you lived approxiately:")
+print("days:",days)
+print("hours:",hours)
+print("minutes:",minutes)
+print("seconds: ",seconds)
