60adc8ee01ccb7e9581cb8b581dde8eab4ece890
Shreeram Patidar <srpatidar6263@gmail.com>
2026-06-21 17:19:00 +0530

    Update and rename Ques11.py to Assignment1/Ques11.py

---
+++ Assignment1/Ques11.py
@@ -0,0 +1,9 @@
+"""
+Greeting Message
+Take name as input and print:
+Hello <name>
+Welcome to Python
+"""
+name=input("Enter Your Name: ")
+print("Hello ",name)
+print("Welcome to the World of Python")
--- Ques11.py
+++
@@ -1,3 +0,0 @@
-name=input("Enter Your Name: ")
-print("Hello ",name)
-print("Welcome to the World of Python")
