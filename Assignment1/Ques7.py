1e862101b44c4e4426431bdd23c63308244b89c4
Shreeram Patidar <srpatidar6263@gmail.com>
2026-06-21 17:14:03 +0530

    Update and rename Ques7.py to Assignment1/Ques7.py

---
+++ Assignment1/Ques7.py
@@ -0,0 +1,8 @@
+"""
+Full Name Display
+Take first name and last name as input and display:
+Full Name: John Doe
+"""
+first_name=input("Enter First Name: ")
+second_name=input("Enter Second Name: ")
+print(first_name+" "+second_name)
--- Ques7.py
+++
@@ -1,3 +0,0 @@
-first_name=input("Enter First Name: ")
-second_name=input("Enter Second Name: ")
-print(first_name+" "+second_name)
