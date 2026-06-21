5a2fd5b0e9dd5fa3a6ed444fa64cbc24f1ed13bc
Shreeram Patidar <srpatidar6263@gmail.com>
2026-06-21 16:55:55 +0530

    Update and rename Ques3.py to Assignment1/Ques3.py

---
+++ Assignment1/Ques3.py
@@ -0,0 +1,21 @@
+"""
+Resume Format
+Write a program to display:
+=== Resume ===
+Name       : Alice Johnson
+Email      : alice@example.com
+Skills     :
+  - Java
+  - HTML & CSS
+  - Problem Solving
+Experience : 2 years at XYZ Ltd.
+"""
+
+print("Resume")
+print("Name : Alice Johnson")
+print("Email : alice@example.com")
+print("Skills :")
+print("-Java")
+print("-HTML&CSS")
+print("-Problem Solving")
+print("Experience: 2 year at XYZ Ltd.")
--- Ques3.py
+++
@@ -1,8 +0,0 @@
-print("Resume")
-print("Name : Alice Johnson")
-print("Email : alice@example.com")
-print("Skills :")
-print("-Java")
-print("-HTML&CSS")
-print("-Problem Solving")
-print("Experience: 2 year at XYZ Ltd.")
