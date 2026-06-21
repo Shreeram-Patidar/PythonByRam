af1cde13b520cf80dbca7e6071bb5e9d87f77c52
Shreeram Patidar <srpatidar6263@gmail.com>
2026-06-21 17:19:45 +0530

    Update and rename Ques12.py to Assignment1/Ques12.py

---
+++ Assignment1/Ques12.py
@@ -0,0 +1,11 @@
+"""
+Favorite Things
+Take input:
+- Favorite food
+- Favorite color
+Display:
+I like <food> and my favorite color is <color>
+"""
+food=input("Your Favorite Food: ")
+color=input("Your Favorite Color: ")
+print("I like ",food," and my favorite color is ",color)
--- Ques12.py
+++
@@ -1,3 +0,0 @@
-food=input("Your Favorite Food: ")
-color=input("Your Favorite Color: ")
-print("I like ",food," and my favorite color is ",color)
