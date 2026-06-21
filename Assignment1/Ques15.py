9b017a2cd6b0eb94618f9596dbe456b934edc793
Shreeram Patidar <srpatidar6263@gmail.com>
2026-06-21 17:22:57 +0530

    Update and rename Ques15.py to Assignment1/Ques15.py

--- Assignment1/Ques15.py
+++ Assignment1/Ques15.py
@@ -1,3 +1,15 @@
+"""
+Bio Data
+Take input:
+- Name
+- Age
+- Phone
+Display:
+--- Bio Data ---
+Name  :
+Age   :
+Phone :
+"""
 name=input("Enter your Name: ")
 age=input("Enter your Age: ")
 phone=input("Enter your Phone: ")
