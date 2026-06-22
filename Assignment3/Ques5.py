843d921834c0e8b621a257b74cecaae41cc407c9
Shreeram Patidar <srpatidar6263@gmail.com>
2026-06-22 07:40:14 +0530

    Update and rename Ques5.py to Assignment3/Ques5.py

---
+++ Assignment3/Ques5.py
@@ -0,0 +1,20 @@
+"""
+Question 5: Average Marks Calculator
+
+Write a Python program that:
+
+Accepts marks of 3 subjects.
+Calculates average.
+
+Input:
+Marks = 80, 90, 70
+
+Output:
+Average = 80.0
+"""
+
+mark1=int(input("Enter the mark1 :"))
+mark2=int(input("Enter the mark2 :"))
+mark3=int(input("Enter the mark3 :"))
+avg=(mark1+mark2+mark3)/3
+print("Average =",avg)
--- Ques5.py
+++
@@ -1,5 +0,0 @@
-mark1=int(input("Enter the mark1 :"))
-mark2=int(input("Enter the mark2 :"))
-mark3=int(input("Enter the mark3 :"))
-avg=(mark1+mark2+mark3)/3
-print("Average =",avg)
