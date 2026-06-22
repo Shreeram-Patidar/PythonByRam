394d7cc52f30ec4698881213a80e76bba4aa3f27
Shreeram Patidar <srpatidar6263@gmail.com>
2026-06-22 07:39:11 +0530

    Update and rename Ques4.py to Assignment3/Ques4.py

---
+++ Assignment3/Ques4.py
@@ -0,0 +1,20 @@
+"""
+Question 4: Area of Rectangle
+
+Write a Python program that:
+
+Accepts length and breadth.
+Calculates area.
+
+Input:
+Length = 10
+Breadth = 5
+
+Output:
+Area = 50
+"""
+
+length=int(input("Enter Length: "))
+breath=int(input("Enter Breath: "))
+area=length*breath
+print("Area: ",area)
--- Ques4.py
+++
@@ -1,4 +0,0 @@
-length=int(input("Enter Length: "))
-breath=int(input("Enter Breath: "))
-area=length*breath
-print("Area: ",area)
