6f7fbd3b23845b93580b22f867ce0ce1e36418d3
Shreeram Patidar <srpatidar6263@gmail.com>
2026-06-22 07:38:05 +0530

    Update and rename Ques3.py to Assignment3/Ques3.py

---
+++ Assignment3/Ques3.py
@@ -0,0 +1,18 @@
+"""
+Question 3: Electricity Bill Calculator
+
+Write a Python program that:
+
+Accepts number of units.
+Calculates bill (₹6 per unit).
+
+Input:
+Units = 100
+
+Output:
+Bill = 600
+"""
+
+unit=int(input("Enter No. of Unit: "))
+bill=unit*6
+print("Bill =",bill)
--- Ques3.py
+++
@@ -1,3 +0,0 @@
-unit=int(input("Enter No. of Unit: "))
-bill=unit*6
-print("Bill =",bill)
