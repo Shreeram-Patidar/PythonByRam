bc31c91bbf2205e12945d33c55c4edc63ddb0969
Shreeram Patidar <srpatidar6263@gmail.com>
2026-06-22 07:50:16 +0530

    Update and rename Ques15.py to Assignment3/Ques15.py

--- Assignment3/Ques15.py
+++ Assignment3/Ques15.py
@@ -1,3 +1,21 @@
+"""
+Question 15: Average Speed for Multiple Trips
+
+Write a Python program that:
+
+Accepts distance1, time1, distance2, time2.
+Calculates average speed.
+
+Input:
+Distance1 = 60
+Time1 = 1
+Distance2 = 40
+Time2 = 1
+
+Output:
+Average Speed = 50 km/h
+"""
+
 distance1=int(input("Enter the first distance :"))
 time1=int(input("Enter the first time"))
 distance2=int(input("Enter the second distance :"))
