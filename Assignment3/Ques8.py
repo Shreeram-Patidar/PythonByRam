25bc8f1184cda8c72a95468ed3495fa5b4ebfa17
Shreeram Patidar <srpatidar6263@gmail.com>
2026-06-22 07:44:05 +0530

    Update and rename Ques8.py to Assignment3/Ques8.py

---
+++ Assignment3/Ques8.py
@@ -0,0 +1,18 @@
+"""
+Question 8: Data Storage Converter
+
+Write a Python program that:
+
+Accepts value in MB.
+Converts into GB.
+
+Input:
+MB = 2048
+
+Output:
+GB = 2.0
+"""
+
+mb=float(input("Enter MB :"))
+gb=mb/1024
+print("GB =",gb)
--- Ques8.py
+++
@@ -1,3 +0,0 @@
-mb=float(input("Enter MB :"))
-gb=mb/1024
-print("GB =",gb)
