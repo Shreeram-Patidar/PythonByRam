90c3ab975785400fd727108b56540558a19aa5f6
Shreeram Patidar <srpatidar6263@gmail.com>
2026-06-22 07:42:13 +0530

    Update and rename Ques7.py to Assignment3/Ques7.py

---
+++ Assignment3/Ques7.py
@@ -0,0 +1,18 @@
+"""
+Question 7: Circle Area Calculator
+
+Write a Python program that:
+
+Accepts radius.
+Calculates area of circle.
+
+Input:
+Radius = 7
+
+Output:
+Area = 153.86
+"""
+
+radius=float(input("Enter Radius :"))
+area=3.14*radius*radius
+print("Area =",area)
