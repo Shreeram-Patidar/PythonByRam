90ebf4ec4ef90fb812d94305a45619a350addfbd
Shreeram Patidar <srpatidar6263@gmail.com>
2026-06-23 12:01:27 +0530

    Delete Ques10.py

--- Ques10.py
+++
@@ -1,9 +0,0 @@
-seconds=int(input("Enter total seconds:"))
-hrs=(seconds)//3600
-seconds=seconds-hrs*3600
-minutes=(seconds)//60
-seconds=seconds-minutes*60
-
-print("Hours :",hrs)
-print("Minutes:",minutes)
-print("Seconds :",seconds)
