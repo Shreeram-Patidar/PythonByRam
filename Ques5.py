ef7cf98b9dd89d3a1255e913f65287f5f63c4766
Shreeram Patidar <srpatidar6263@gmail.com>
2026-06-23 12:03:54 +0530

    Delete Ques5.py

--- Ques5.py
+++
@@ -1,8 +0,0 @@
-salary=int(input("Monthly salary ="))
-days=int(input("Working days ="))
-hour=int(input("Working hour per day ="))
-per_day=salary/days
-per_hour=per_day/8
-
-print("Salary per Day :",per_day)
-print("Salary per hours :",per_hour)
