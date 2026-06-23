ff2fc7d02abe3e0499c2d3d34526755485e0dd1c
Shreeram Patidar <srpatidar6263@gmail.com>
2026-06-23 18:47:53 +0530

    Update Ques8.py

--- Assignment4/Ques8.py
+++ Assignment4/Ques8.py
@@ -1,3 +1,17 @@
+"""
+Question 8: Compound Interest
+
+A person invests money in a bank that provides compound interest annually.
+
+Input:
+Principal = 10000
+Rate = 5%
+Time = 2 years
+
+Expected Output:
+Amount after interest = 11025.0
+"""
+
 p,r,t,=map(int,input("enter principle rate or time").split())
 total_amount=p*(1+r/100)**t
 print("Total amount :",total_amount)
