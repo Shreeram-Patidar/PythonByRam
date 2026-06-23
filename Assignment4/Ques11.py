1a5728e83cb654f8f487280adddb7d288c52b861
Shreeram Patidar <srpatidar6263@gmail.com>
2026-06-23 18:50:02 +0530

    Update Ques11.py

--- Assignment4/Ques11.py
+++ Assignment4/Ques11.py
@@ -1,2 +1,11 @@
+"""
+Question 11: Expression Evaluation
+
+A billing system applies nested calculations with discounts and extra charges using brackets and unary operators.
+
+Input:
+50 + (10 * (+(2**3))) / 4 - (-6 % 4)
+"""
+
 x=50+(10*(+(2**3)))/4-(-6%4)
 print(x)
