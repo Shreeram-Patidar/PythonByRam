4dd8cfc154063f0588c3a610d3d296967e702e24
Shreeram Patidar <srpatidar6263@gmail.com>
2026-06-23 12:02:53 +0530

    Delete Ques9.py

--- Ques9.py
+++
@@ -1,5 +0,0 @@
-d,m,p=map(int,input("Enter distance mileage petrol price").split())
-petrol_used = d/m
-total_cost = petrol_used*p
-print("petrol used: ",petrol_used)
-print("Total Cost :",total_cost)
