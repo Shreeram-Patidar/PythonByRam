184483f8b0f20b267e39c16de0d98382b31eda66
Shreeram Patidar <srpatidar6263@gmail.com>
2026-06-23 18:48:40 +0530

    Update Ques9.py

--- Assignment4/Ques9.py
+++ Assignment4/Ques9.py
@@ -1,3 +1,18 @@
+"""
+Question 9: Petrol Cost Calculation
+
+You traveled a certain distance. Based on mileage and petrol price, calculate fuel used and total cost.
+
+Input:
+Distance = 450 km
+Mileage = 15 km/litre
+Petrol price = 110/litre
+
+Expected Output:
+Petrol Used = 30.0 litres
+Total Cost = 3300.0
+"""
+
 d,m,p=map(int,input("Enter distance mileage petrol price").split())
 petrol_used = d/m
 total_cost = petrol_used*p
