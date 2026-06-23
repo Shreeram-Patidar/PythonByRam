10a5b5553c0e62a2ebad8572adfa062563b51382
Shreeram Patidar <srpatidar6263@gmail.com>
2026-06-23 18:45:52 +0530

    Update Ques6.py

--- Assignment4/Ques6.py
+++ Assignment4/Ques6.py
@@ -1,3 +1,16 @@
+"""
+Question 6: Data Storage Conversion
+
+A user wants to convert data from GB into MB and KB.
+
+Input:
+Data = 5 GB
+
+Expected Output:
+In MB = 5120.0
+In KB = 5242880.0
+"""
+
 gb=int(input("Data uses in GB:"))
 mb=gb*1024
 kb=mb*1024
