6d61f535f62c36a67f6be24869a7b5693d5de04f
Shreeram Patidar <srpatidar6263@gmail.com>
2026-06-23 18:43:33 +0530

    Update Ques3.py

--- Assignment4/Ques3.py
+++ Assignment4/Ques3.py
@@ -1,3 +1,17 @@
+"""
+Question 3: Student Marks Analysis
+
+A student wants to calculate total marks, average, and percentage from 5 subjects.
+
+Input:
+Marks = 78, 85, 90, 88, 80
+
+Expected Output:
+Total = 421
+Average = 84.2
+Percentage = 84.2
+"""
+
 a,b,c,d,e=map(int,input("Enter marks ").split())
 total=a+b+c+d+e
 avg=total/5
